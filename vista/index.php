23e3e3fc1c4d60cf91a6c249c546391dd229d649
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-03-31 11:49:02 -0300

    footer agregado

--- vista/index.php
+++ vista/index.php
@@ -16,5 +16,7 @@
     <div class="col-lg-8 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .75;">Tabla</div>
     <div class="col-lg-4 col-sm-4 bg-primary p-3">Parrafo</div>
 </div>
-
+<div class="bg-success row p-3" style="--bs-bg-opacity: .25;">
+    Footer
+</div>
 <?php include_once('common/footer.php') ?>
