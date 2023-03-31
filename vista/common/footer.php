493cc68f5b376fb1fbaaeef6ff237ceb1a03786f
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-03-31 18:12:53 -0300

    carousel

--- vista/common/footer.php
+++ vista/common/footer.php
@@ -1,5 +1,8 @@
-</div> 
+</div>
 <!--Fin del div.container-fluid-->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
-</body>
-</html>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+    </body> 
+    </html>
--- vista/index.php
+++ vista/index.php
@@ -5,50 +5,96 @@
 <div class="bg-success row p-3" style="--bs-bg-opacity: .5;">
     Fila (col-lg-12)
 </div>
-<div class="bg-success row p-3">
-    <nav>
-        <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <button class="nav-link active" id="nav0-tab" data-bs-toggle="tab" data-bs-target="#nav0" type="button" role="tab" aria-controls="nav0" aria-selected="true">
-                <img src="Assets/Img/queespumse2.jpg" alt="" style="width: 100px;">
-                Categoria 0
+
+<main class="">
+    <div class="row m-3">
+        <div id="carouselExampleCaptions" class="carousel carousel-fade col-12 mb-3">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner nav"><!-- CONTENEDOR PADRE DEBE TENER CLASE NAV -->
+                <div class="carousel-item active">
+                    <div class="nav-link" data-bs-toggle="tab" data-bs-target="#home-tab-pane">
+                        <img src="./Assets/Img/1.png" class="d-block h-20" alt="..." />
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="nav-link" data-bs-toggle="tab" data-bs-target="#profile-tab-pane">
+                        <img src="./Assets/Img/2.jpg" class="d-block h-20" alt="..." />
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="nav-link" data-bs-toggle="tab" data-bs-target="#contact-tab-pane">
+                        <img src="./Assets/Img/queespumse2.jpg" class="d-block h-20" alt="..." />
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
             </button>
-            <button class="nav-link" id="nav1-tab" data-bs-toggle="tab" data-bs-target="#nav1" type="button" role="tab" aria-controls="nav1" aria-selected="false">
-                <img src="Assets/Img/queespumse2.jpg" alt="" style="width: 100px;">
-                Categoria 1
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
             </button>
         </div>
-    </nav>
-</div>
+
+        <div class="col-12 my-5">
+            <div class="tab-content">
+
+                <div class="tab-pane fade show active" id="home-tab-pane">
+                    <div class="row">
+                        <div class="col-lg-8 col-sm-4 bg-primary border-1 border-dark p-3">
+                            Informacion
+                        </div>
+                        <div class="col-lg-4 col-sm-8 bg-primary border-1 border-dark p-3">
+                            Imagen expandida
+                        </div>
+                    </div>
+                </div>
 
 
-<div class="tab-content row" id="nav-tabContent">
-    <div class="tab-pane fade show active " id="nav0" role="tabpanel" aria-labelledby="nav0-tab" tabindex="0">
-        <div class="row">
-            <div class="col-lg-8 col-sm-4 bg-primary p-3" style="--bs-bg-opacity: .25;">Informacion 0</div>
-            <div class="col-lg-4 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .50;">Imagen expandida 0</div>
-        </div>
-        <div class="row">
-            <div class="col-lg-8 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .75;">Tabla 0</div>
-            <div class="col-lg-4 col-sm-4 bg-primary p-3">Parrafo 0</div>
+                <div class="tab-pane fade" id="profile-tab-pane">
+                    <div class="row">
+                        <div class="col-lg-8 col-sm-4 bg-warning border-1 border-dark p-3">
+                            Informacion
+                        </div>
+                        <div class="col-lg-4 col-sm-8 bg-warning border-1 border-dark p-3">
+                            Imagen expandida
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="tab-pane fade" id="contact-tab-pane">
+                    <div class="row">
+                        <div class="row">
+                            <div class="col-lg-8 col-sm-4 bg-success p-3" style="--bs-bg-opacity: .25;" id="informacion">informacion</div>
+                            <div class="col-lg-4 col-sm-8 bg-success p-3" style="--bs-bg-opacity: .50;">
+                                <img src="Assets/Img/1.png" alt="" class="img-fluid" id="imagenExpandida">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     </div>
-    <div class="tab-pane fade" id="nav1" role="tabpanel" aria-labelledby="nav1-tab" tabindex="0">
-        <div class="row">
-            <div class="col-lg-8 col-sm-4 bg-primary p-3" style="--bs-bg-opacity: .25;">Informacion 1</div>
-            <div class="col-lg-4 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .50;">Imagen expandida 1</div>
-        </div>
-        <div class="row">
-            <div class="col-lg-8 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .75;">Tabla 1</div>
-            <div class="col-lg-4 col-sm-4 bg-primary p-3">Parrafo 1</div>
-        </div>
-    </div>
+</main>
 
 
-
+<div class="row">
+    <div class="col-lg-8 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .75;" id="tabla">Tabla</div>
+    <div class="col-lg-4 col-sm-4 bg-primary p-3" id="parrafo">Parrafo</div>
 </div>
+
 
 
 <div class="bg-success row p-3" style="--bs-bg-opacity: .25;">
     Footer
 </div>
+<script src="Assets/Js/script.js"></script>
 <?php include_once('common/footer.php') ?>
