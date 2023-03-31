c1e57310f27b1cd1ab89724e2befb0ed5acfd2a0
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-03-31 19:21:06 -0300

    ..

--- vista/common/header.php
+++ vista/common/header.php
@@ -6,13 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trabajo</title>
-    <!doctype html>
+
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="../Assets/Css/style.css">
 
 </head>
 
 <body>
-    <div class="container-fluid ">
+    <div class="container-fluid">
