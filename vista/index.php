e2090ef887ce4f23e2c435b10ac8c0e554a023be
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-03-31 11:45:21 -0300

    enunciado agregado

--- vista/index.php
+++ vista/index.php
@@ -1,20 +1,20 @@
 <?php include_once('common/header.php') ?>
-<div class="bg-success row" style="--bs-bg-opacity: .25;">
+<div class="bg-success row p-3" style="--bs-bg-opacity: .25;">
     Encabezado
 </div>
-<div class="bg-success row" style="--bs-bg-opacity: .5;">
+<div class="bg-success row p-3" style="--bs-bg-opacity: .5;">
     Fila (col-lg-12)
 </div>
-<div class="bg-success row">
+<div class="bg-success row p-3">
     Fila (imagenes)
 </div>
 <div class="row">
-    <div class="col col-sm-4 bg-primary" style="--bs-bg-opacity: .25;">Informacion</div>
-    <div class="col col-sm-8 bg-primary" style="--bs-bg-opacity: .50;">Imagen expandida</div>
+    <div class="col-lg-8 col-sm-4 bg-primary p-3" style="--bs-bg-opacity: .25;">Informacion</div>
+    <div class="col-lg-4 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .50;">Imagen expandida</div>
 </div>
 <div class="row">
-    <div class="col col-sm-8 bg-primary" style="--bs-bg-opacity: .75;">Tabla</div>
-    <div class="col col-sm-4 bg-primary">Parrafo</div>
+    <div class="col-lg-8 col-sm-8 bg-primary p-3" style="--bs-bg-opacity: .75;">Tabla</div>
+    <div class="col-lg-4 col-sm-4 bg-primary p-3">Parrafo</div>
 </div>
 
 <?php include_once('common/footer.php') ?>
