3082d692c05efdac7bb06d9f4bd77007ceae5adc
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-04-03 23:17:32 -0300

    carousel nuevo

--- vista/index.php
+++ vista/index.php
@@ -1,4 +1,4 @@
-<?php include_once('common/header.php') ?>
+<?php include_once('../vista/common/header.php') ?>
 <div class="row p-3 justify-content-center ">
     <div class="col-12 justify-content-center">
         <img src="Assets/Img/World_Taekwondo.png" alt="" width="100px" />
@@ -14,15 +14,6 @@
 
 </div>
 
-<<<<<<< Updated upstream
-<main class="">
-    <div class="row text-bg-danger">
-        <div id="carouselExampleCaptions" data-bs-ride="carousel" class="carousel carousel-fade  mx-auto col-lg-5 col-sm-12 mb-3">
-            <div class="carousel-indicators mb-0">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-=======
 
 
 <div class="text-center my-3">
@@ -139,58 +130,17 @@
                         </div>
                     </div>
                 </div>
-
->>>>>>> Stashed changes
-            </div>
-            <div class="carousel-inner nav "><!-- CONTENEDOR PADRE DEBE TENER CLASE NAV -->
-                <div class="carousel-item active " data-bs-interval="2000">
-                    <div class="d-flex justify-content-center">
-                        <div class="nav-link" data-bs-toggle="tab" data-bs-target="#infantilB">
-                            <img src="./Assets/Img/thumbnails/infantiles b.jpg" class="rounded" alt="..." height="100px" />
-                            <div class="pe-none carousel-caption">
-                                <h5>Infantiles B</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <div class="d-flex justify-content-center">
-                        <div class="nav-link" data-bs-toggle="tab" data-bs-target="#infantilA">
-                            <img src="./Assets/Img/thumbnails/infantiles a.jpg" class="d-block rounded" alt="..." height="100px" />
-                            <div class="pe-none carousel-caption d-none d-md-block">
-                                <h5>Infantiles A</h5>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="carousel-item" data-bs-interval="2000">
-                    <div class="d-flex justify-content-center">
-                        <div class="nav-link" data-bs-toggle="tab" data-bs-target="#cadete">
-                            <img src="./Assets/Img/thumbnails/cadete 12-14.jpg" class="d-block rounded" alt="..." height="100px" />
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                
+            <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            </a>
+            <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-
-
+            </a>
+        </div>    
     </div>
-<<<<<<< Updated upstream
-=======
 </div>
 
->>>>>>> Stashed changes
     <div class="row">
         <div class="col-12 my-5">
             <div class="tab-content fs-5">
@@ -240,6 +190,4 @@
 </main>
 
 
-
-<script src="Assets/Js/script.js"></script>
-<?php include_once('common/footer.php') ?>
+<?php include_once('../vista/common/footer.php') ?>
