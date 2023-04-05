4d61ee08ac3ba5b1c0bd501f4e674606a9084210
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-04-05 14:41:06 -0300

    Ajustes tablas responsive

--- vista/tabInfantilesB.php
+++ vista/tabInfantilesB.php
@@ -5,7 +5,9 @@
         <hr>
         <h4>Infantil B</h4>
         <p>
-            En la competencia de poomsae reconocido elite, los competidores demostraron sus habilidades en el taekwondo con movimientos precisos y potentes. Los jueces evaluaron la técnica y la precisión, y los fanáticos apoyaron con entusiasmo. Después de muchas rondas, un ganador fue coronado, celebrando su increíble habilidad y dedicación.
+            En la competencia de poomsae reconocido elite, los competidores demostraron sus habilidades en el taekwondo 
+            con movimientos precisos y potentes. Los jueces evaluaron la técnica y la precisión, y los fanáticos apoyaron 
+            con entusiasmo. Después de muchas rondas, un ganador fue coronado, celebrando su increíble habilidad y dedicación.
         </p>
     </div>
 
@@ -20,10 +22,10 @@
         <table class="table text-center m-auto bg-light rounded">
             <thead>
                 <tr>
-                    <th scope="col">
+                    <th scope="col"  class="d-none d-sm-table-cell">
                         <div class="">N°</div>
                     </th>
-                    <th scope="col">
+                    <th scope="col"  class="d-none d-sm-table-cell">
                         <div class="">Lugar</div>
                     </th>
                     <th scope="col">
@@ -36,40 +38,47 @@
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg> Puntaje</div>
                     </th>
+                    <th scope="col">
+                        <div>Posicion</div>
+                    </th>
                 </tr>
             </thead>
             <tbody class="table-group-divider">
                 <tr>
-                    <th scope="row">183</th>
-                    <th>Neuquén</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">183</th>
+                    <th class="d-none d-sm-table-cell">Neuquén</th>
                     <td>Julián Serrano</td>
                     <th>5,13</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">1° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">342</th>
-                    <th>Tucumán</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">342</th>
+                    <th class="d-none d-sm-table-cell">Tucumán</th>
                     <td>Joaquín Pelusso</td>
                     <th>4,89</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">2° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">57</th>
-                    <th>Formosa</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">57</th>
+                    <th class="d-none d-sm-table-cell">Formosa</th>
                     <td>Pepe Argento</td>
                     <th>4,5</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">3° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">129</th>
-                    <th>Jujuy</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">129</th>
+                    <th class="d-none d-sm-table-cell">Jujuy</th>
                     <td>Cristóbal Belmar Careau</td>
                     <th>4,17</th>
                 </tr>
             </tbody>
         </table>
     </div>
-    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">Julian Serrano es el ganador de Poomsae Elite Individual en la categoría Infantil A. Con una gran habilidad técnica y física, mostró su talento en la competencia. "El secreto del éxito es la dedicación y la pasión", dijo Julian después de su victoria.</div>
+    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">
+        Julian Serrano es el ganador de Poomsae Elite Individual en la categoría Infantil A. 
+        Con una gran habilidad técnica y física, mostró su talento en la competencia. 
+        "El secreto del éxito es la dedicación y la pasión", dijo Julian después de su victoria.
+    </div>
 </div>
 
 <div id="infoFeminfantiles b" class="row">
@@ -77,11 +86,11 @@
         <table class="table text-center m-auto bg-light rounded">
             <thead>
                 <tr>
-                    <th scope="col">
-                        <div class="">N°</div>
+                    <th scope="col" class=" d-none d-sm-table-cell">
+                        <div>N°</div>
                     </th>
-                    <th scope="col">
-                        <div class="">Lugar</div>
+                    <th scope="col" class=" d-none d-sm-table-cell">
+                        <div>Lugar</div>
                     </th>
                     <th scope="col">
                         <div class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
@@ -92,6 +101,9 @@
                         <div class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg> Puntaje</div>
+                    </th>
+                    <th scope="col">
+                        <div>Posicion</div>
                     </th>
                 </tr>
             </thead>
@@ -100,31 +112,35 @@
                     <th scope="row" class="d-none d-sm-table-cell">9</th>
                     <th class="d-none d-sm-table-cell">Mendoza</th>
                     <td>Romina González</td>
-                    <th class="d-none d-sm-table-cell">7.65</th>
+                    <th class="">7.65</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">1° Lugar</span></th>
                 </tr>
                 <tr>
                     <th scope="row" class="d-none d-sm-table-cell">5</th>
                     <th class="d-none d-sm-table-cell">Córdoba</th>
                     <td>Valentina García</td>
-                    <th class="d-none d-sm-table-cell">7.23</th>
+                    <th class="">7.23</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">2° Lugar</span></th>
                 </tr>
                 <tr>
                     <th scope="row" class="d-none d-sm-table-cell">2</th>
                     <th class="d-none d-sm-table-cell">Buenos Aires</th>
                     <td>Lucía Sánchez</td>
-                    <th class="d-none d-sm-table-cell">6.57</th>
+                    <th class="">6.57</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">3° Lugar</span></th>
                 </tr>
                 <tr>
                     <th scope="row" class="d-none d-sm-table-cell">7</th>
                     <th class="d-none d-sm-table-cell">Santa Fe</th>
                     <td>Agustina Fernández</td>
-                    <th class="d-none d-sm-table-cell">6.02</th>
+                    <th class="">6.02</th>
                 </tr>
             </tbody>
         </table>
     </div>
-    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">Romina Gonzales es la ganadora de la categoría en la última competencia. Con su dedicación y habilidades logró impresionar a todos los jueces con su desempeño. "Siempre creí en mí misma, y eso me llevó a donde estoy hoy", dijo Romina después de su victoria.</div>
+    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">
+        Romina Gonzales es la ganadora de la categoría en la última competencia. 
+        Con su dedicación y habilidades logró impresionar a todos los jueces con su desempeño. 
+        "Siempre creí en mí misma, y eso me llevó a donde estoy hoy", dijo Romina después de su victoria.
+    </div>
 </div>
--- vista/tabsenior1.php
+++ vista/tabsenior1.php
@@ -15,14 +15,14 @@
 </div>
 <hr>
 <div id="infoMascsenior 1 menos 50" class="row d-none">
-    <div class="col-lg-8 col-sm-8 p-3 table-responsive-sm">
+<div class="col-lg-8 col-sm-8 p-3 table-responsive-sm">
         <table class="table text-center m-auto bg-light rounded">
             <thead>
                 <tr>
-                    <th scope="col">
+                    <th scope="col"  class="d-none d-sm-table-cell">
                         <div class="">N°</div>
                     </th>
-                    <th scope="col">
+                    <th scope="col"  class="d-none d-sm-table-cell">
                         <div class="">Lugar</div>
                     </th>
                     <th scope="col">
@@ -35,52 +35,59 @@
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg> Puntaje</div>
                     </th>
+                    <th scope="col">
+                        <div>Posicion</div>
+                    </th>
                 </tr>
             </thead>
             <tbody class="table-group-divider">
                 <tr>
-                    <th scope="row">183</th>
-                    <th>Neuquén</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">183</th>
+                    <th class="d-none d-sm-table-cell">Neuquén</th>
                     <td>Julián Serrano</td>
                     <th>5,13</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">1° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">342</th>
-                    <th>Tucumán</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">342</th>
+                    <th class="d-none d-sm-table-cell">Tucumán</th>
                     <td>Joaquín Pelusso</td>
                     <th>4,89</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">2° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">57</th>
-                    <th>Formosa</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">57</th>
+                    <th class="d-none d-sm-table-cell">Formosa</th>
                     <td>Pepe Argento</td>
                     <th>4,5</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">3° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">129</th>
-                    <th>Jujuy</th>
+                    <th scope="row"  class="d-none d-sm-table-cell">129</th>
+                    <th class="d-none d-sm-table-cell">Jujuy</th>
                     <td>Cristóbal Belmar Careau</td>
                     <th>4,17</th>
                 </tr>
             </tbody>
         </table>
     </div>
-    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">Julian Serrano es el ganador de Poomsae Elite Individual en la categoría Infantil A. Con una gran habilidad técnica y física, mostró su talento en la competencia. "El secreto del éxito es la dedicación y la pasión", dijo Julian después de su victoria.</div>
+    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">
+        Julian Serrano es el ganador de Poomsae Elite Individual en la categoría Infantil A. 
+        Con una gran habilidad técnica y física, mostró su talento en la competencia. 
+        "El secreto del éxito es la dedicación y la pasión", dijo Julian después de su victoria.
+    </div>
 </div>
 
 <div id="infoFemsenior 1 menos 50" class="row">
-    <div class="col-lg-8 col-sm-8 p-3 table-responsive-sm">
+<div class="col-lg-8 col-sm-8 p-3 table-responsive-sm">
         <table class="table text-center m-auto bg-light rounded">
             <thead>
                 <tr>
-                    <th scope="col">
-                        <div class="">N°</div>
+                    <th scope="col" class=" d-none d-sm-table-cell">
+                        <div>N°</div>
                     </th>
-                    <th scope="col">
-                        <div class="">Lugar</div>
+                    <th scope="col" class=" d-none d-sm-table-cell">
+                        <div>Lugar</div>
                     </th>
                     <th scope="col">
                         <div class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
@@ -91,6 +98,9 @@
                         <div class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg> Puntaje</div>
+                    </th>
+                    <th scope="col">
+                        <div>Posicion</div>
                     </th>
                 </tr>
             </thead>
@@ -99,31 +109,34 @@
                     <th scope="row" class="d-none d-sm-table-cell">9</th>
                     <th class="d-none d-sm-table-cell">Mendoza</th>
                     <td>Romina González</td>
-                    <th class="d-none d-sm-table-cell">7.65</th>
+                    <th class="">7.65</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">1° Lugar</span></th>
                 </tr>
                 <tr>
                     <th scope="row" class="d-none d-sm-table-cell">5</th>
                     <th class="d-none d-sm-table-cell">Córdoba</th>
                     <td>Valentina García</td>
-                    <th class="d-none d-sm-table-cell">7.23</th>
+                    <th class="">7.23</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">2° Lugar</span></th>
                 </tr>
                 <tr>
                     <th scope="row" class="d-none d-sm-table-cell">2</th>
                     <th class="d-none d-sm-table-cell">Buenos Aires</th>
                     <td>Lucía Sánchez</td>
-                    <th class="d-none d-sm-table-cell">6.57</th>
+                    <th class="">6.57</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">3° Lugar</span></th>
                 </tr>
                 <tr>
                     <th scope="row" class="d-none d-sm-table-cell">7</th>
                     <th class="d-none d-sm-table-cell">Santa Fe</th>
                     <td>Agustina Fernández</td>
-                    <th class="d-none d-sm-table-cell">6.02</th>
+                    <th class="">6.02</th>
                 </tr>
             </tbody>
         </table>
     </div>
-    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">Romina Gonzales es la ganadora de la categoría en la última competencia. Con su dedicación y habilidades logró impresionar a todos los jueces con su desempeño. "Siempre creí en mí misma, y eso me llevó a donde estoy hoy", dijo Martina después de su victoria.</div>
-</div>
+    <div class="col-lg-4 col-sm-4 p-3" id="parrafo">
+        Romina Gonzales es la ganadora de la categoría en la última competencia. 
+        Con su dedicación y habilidades logró impresionar a todos los jueces con su desempeño. 
+        "Siempre creí en mí misma, y eso me llevó a donde estoy hoy", dijo Romina después de su victoria.
+    </div></div>
