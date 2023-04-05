158d604182b2a2d215f230bdc3e7a7a3aa94c20b
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-04-05 11:11:38 -0300

    tabla responsive

--- vista/tabInfantilesA.php
+++ vista/tabInfantilesA.php
@@ -2,27 +2,27 @@
     <div class="col-lg-4 col-sm-4 p-3">
         <button class="btn btn-secondary" id="fem" onclick="miFuncion('fem','infantiles a')">Femenino</button>
         <button class="btn btn-secondary" id="masc" onclick="miFuncion('masc','infantiles a')">Masculino</button>
-        
+        <hr>
         <h4>Infantil A</h4>
         <p>
-            La categoría infantil A de Poomsae reconocido elite individual está diseñada para niños de 8 y 9 años y requiere precisión, fuerza, resistencia y concentración en una serie de movimientos y posturas elegantes. Es una oportunidad emocionante y gratificante para que los jóvenes competidores muestren su habilidad en el taekwondo.
+            En la competencia de poomsae reconocido elite, los competidores demostraron sus habilidades en el taekwondo con movimientos precisos y potentes. Los jueces evaluaron la técnica y la precisión, y los fanáticos apoyaron con entusiasmo. Después de muchas rondas, un ganador fue coronado, celebrando su increíble habilidad y dedicación.
         </p>
     </div>
-    
-    <div class="col-lg-8 col-sm-8 " style="--bs-bg-opacity: .50;" >
+
+    <div class="col-lg-8 col-sm-8 " style="--bs-bg-opacity: .50;">
         <img id="imagen infantiles a" src="./Assets/Img/img/infantiles a f.jpg" alt="" class="img-fluid rounded" id="imagenExpandida">
     </div>
 </div>
 <hr>
 <div class="row">
-    <div class="col-lg-8 col-sm-8 p-3" style="--bs-bg-opacity: .75;" id="tabla">
-        <table id="tabla" class="table-responsive text-center m-auto bg-light rounded" style="width: 650px;">
+    <div class="col-lg-8 col-sm-8 p-3 table-responsive" style="--bs-bg-opacity: .75;" id="tabla">
+        <table id="tabla" class="table text-center m-auto bg-light rounded tablaResultado">
             <thead>
                 <tr>
-                    <th scope="col">
+                    <th scope="col" class="d-none d-sm-table-cell">
                         <div class="">N°</div>
                     </th>
-                    <th scope="col">
+                    <th scope="col " class="d-none d-sm-table-cell">
                         <div class="">Lugar</div>
                     </th>
                     <th scope="col">
@@ -30,7 +30,7 @@
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg> Nombre</div>
                     </th>
-                    <th scope="col">
+                    <th scope="col" class="d-none d-sm-table-cell">
                         <div class=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg> Puntaje</div>
@@ -39,31 +39,31 @@
             </thead>
             <tbody class="table-group-divider">
                 <tr>
-                    <th scope="row">183</th>
-                    <th>Neuquén</th>
+                    <th scope="row" class="d-none d-sm-table-cell">183</th>
+                    <th class="d-none d-sm-table-cell">Neuquén</th>
                     <td>Julián Serrano</td>
-                    <th>5,13</th>
+                    <th class="d-none d-sm-table-cell">5,13</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">1° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">342</th>
-                    <th>Tucumán</th>
-                    <td>Joaquín Pelusso</td>
-                    <th>4,89</th>
+                    <th scope="row" class="d-none d-sm-table-cell">342</th>
+                    <th class="d-none d-sm-table-cell">Tucumán</th>
+                    <td >Joaquín Pelusso</td>
+                    <th class="d-none d-sm-table-cell">4,89</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">2° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">57</th>
-                    <th>Formosa</th>
+                    <th scope="row" class="d-none d-sm-table-cell">57</th>
+                    <th class="d-none d-sm-table-cell">Formosa</th>
                     <td>Pepe Argento</td>
-                    <th>4,5</th>
+                    <th class="d-none d-sm-table-cell">4,5</th>
                     <th class="position-relative"><span class="position-absolute top-50 start-75 translate-middle badge bg-danger">3° Lugar</span></th>
                 </tr>
                 <tr>
-                    <th scope="row">129</th>
-                    <th>Jujuy</th>
+                    <th scope="row" class="d-none d-sm-table-cell">129</th>
+                    <th class="d-none d-sm-table-cell">Jujuy</th>
                     <td>Cristóbal Belmar Careau</td>
-                    <th>4,17</th>
+                    <th class="d-none d-sm-table-cell">4,17</th>
                 </tr>
             </tbody>
         </table>
