11a238d03e2cb10de6866344bfed20f9504e9124
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-04-05 15:17:20 -0300

    favicon agregrado

--- vista/common/header.php
+++ vista/common/header.php
@@ -7,6 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Poomsae Reconocido Individual Elite</title>
 
+    <!-- ICON -->
+    <link rel="icon" type="image\x-icon" href=".\Assets\Img\img\logo.ico">
+
 
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
