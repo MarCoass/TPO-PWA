dfefa266079bd69b18d66c98e9d4e9b5d7cbf137
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-04-05 15:34:12 -0300

    cambie el texto del ganador

--- vista/tabmaster1.php
+++ vista/tabmaster1.php
@@ -136,7 +136,7 @@
         </table>
     </div>
     <div class="col-lg-4 col-sm-4 p-3" id="parrafo">
-        Romina Gonzales es la ganadora de la categoría en la última competencia. 
+        Romina Gonzales es la ganadora de la categoría Master 1 en la última competencia. 
         Con su dedicación y habilidades logró impresionar a todos los jueces con su desempeño. 
         "Siempre creí en mí misma, y eso me llevó a donde estoy hoy", dijo Romina después de su victoria.
     </div></div>
