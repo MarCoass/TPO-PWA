a7974ddd1659eb36e184a6203c3062817a54469c
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-04-12 20:55:50 -0300

    formulario pobre

---
+++ TP2/form.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario simple basico</title>
+</head>
+<body>
+<form>
+  <label for="legajo">Legajo:</label>
+  <input type="text" id="legajo" name="legajo"><br><br>
+
+  <label for="apellido">Apellido:</label>
+  <input type="text" id="apellido" name="apellido"><br><br>
+
+  <label for="nombre">Nombre:</label>
+  <input type="text" id="nombre" name="nombre"><br><br>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email"><br><br>
+
+  <label for="edad">Edad:</label>
+  <input type="number" id="edad" name="edad"><br><br>
+
+  <label for="pais">País:</label>
+  <input type="text" id="pais" name="pais"><br><br>
+
+  <label for="genero">Género:</label>
+  <select id="genero" name="genero">
+    <option value="">Selecciona una opción</option>
+    <option value="masculino">Masculino</option>
+    <option value="femenino">Femenino</option>
+    <option value="otro">Otro</option>
+  </select><br><br>
+
+  <label for="ranking">Ranking:</label>
+  <input type="number" id="ranking" name="ranking"><br><br>
+
+  <input type="submit" value="Enviar">
+</form>
+</body>
+</html>
