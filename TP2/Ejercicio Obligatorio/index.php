93078e422c535027c31b85d08b307aec4dd85f1f
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-04-14 20:12:23 -0300

    buttons ya no son trol xd sirven para cambiar el tab

--- TP2/Ejercicio Obligatorio/index.php
+++ TP2/Ejercicio Obligatorio/index.php
@@ -17,13 +17,13 @@
     </div>
     <div class="col-8 position-relative">
         <div class="nav justify-content-center">
-            <div class="nav-link active" data-bs-toggle="tab" data-bs-target="#paso1">
+            <div class="nav-link active" id="link1" data-bs-toggle="tab" data-bs-target="#paso1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-1-circle" viewBox="0 0 16 16">
                     <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z" />
                 </svg>
             </div>
             <span class="mx-3"></span>
-            <div class="nav-link" data-bs-toggle="tab" data-bs-target="#paso2">
+            <div class="nav-link" id="link2" data-bs-toggle="tab" data-bs-target="#paso2">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-2-circle" viewBox="0 0 16 16">
                     <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6.646 6.24v.07H5.375v-.064c0-1.213.879-2.402 2.637-2.402 1.582 0 2.613.949 2.613 2.215 0 1.002-.6 1.667-1.287 2.43l-.096.107-1.974 2.22v.077h3.498V12H5.422v-.832l2.97-3.293c.434-.475.903-1.008.903-1.705 0-.744-.557-1.236-1.313-1.236-.843 0-1.336.615-1.336 1.306Z" />
                 </svg>
@@ -31,7 +31,7 @@
         </div>
         <form id="myForm"> <!-- INICIO FORM -->
             <div class="tab-content">
-                <div class="tab-pane fade show active" id="paso1" role="tabpanel" aria-labelledby="paso1-tab"> <!-- INICIO CONTENIDO PASO 1 -->
+                <div class="tab-pane fade show active" id="paso1"> <!-- INICIO CONTENIDO PASO 1 -->
                     <div class="row">
                         <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
                             <label class="form-label" for="apellido">Apellido:</label>
@@ -74,7 +74,7 @@
                         </div>
                     </div>
                     <div class="position-absolute bottom-0 end-0 mb-3 me-3">
-                        <button class="btn btn-outline-primary">
+                        <button type="button" class="btn btn-outline-primary" onclick="showTab('paso2', 'link2', 'link1')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg>
@@ -82,7 +82,7 @@
                         </button>
                     </div>
                 </div> <!-- FIN CONTENIDO PASO 1 -->
-                <div class="tab-pane fade" id="paso2" role="tabpanel" aria-labelledby="paso2-tab"> <!-- INICIO CONTENIDO PASO 2 -->
+                <div class="tab-pane fade" id="paso2"> <!-- INICIO CONTENIDO PASO 2 -->
                     <div class="row">
                         <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
                             <label class="form-label" for="legajo">Legajo:</label>
@@ -99,13 +99,13 @@
                         </div>
                     </div>
                     <div class="position-absolute bottom-0 end-0 mb-3 me-3 mt-5">
-                        <button class="btn btn-outline-secondary align-middle">
+                        <button type="button" class="btn btn-outline-secondary" onclick="showTab('paso1', 'link1', 'link2')">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                             </svg>
                             Volver
                         </button>
-                        <button class="btn btn-outline-success">
+                        <button type="submit" class="btn btn-outline-success">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus-fill" viewBox="0 0 16 16">
                                 <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z" />
                             </svg>
@@ -118,6 +118,27 @@
     </div>
 </div>
 
+<script>
+    function showTab(tabId, link1, link2) {
+        // Obtener el tab actual
+        var currentTab = document.querySelector('.tab-pane.active');
+
+        // Ocultar el tab actual
+        currentTab.classList.remove('active');
+        currentTab.classList.remove('show');
+
+        // Mostrar el tab correspondiente al ID
+        var newTab = document.getElementById(tabId);
+        newTab.classList.add('active');
+        newTab.classList.add('show');
+
+        // Actualizar los nav-links
+        var enlace1 = document.getElementById(link1);
+        enlace1.classList.add('active');
+        var enlace2 = document.getElementById(link2);
+        enlace2.classList.remove('active');
+    }
+</script>
 
 
 <!--<form class="row m-5" id="myForm">
