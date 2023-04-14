4854c5b28528d85df222cc3fef76c0ccec13e93d
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-04-14 18:57:16 -0300

    restructuracion sitio, nuevo form? se vienen cositas

---
+++ TP2/Ejercicio Obligatorio/index.php
@@ -0,0 +1,242 @@
+<?php include_once('./estructura/header.php') ?>
+<div class="row p-3 text-light bg-seccion2">
+    <div class="text-center">
+        <span class="display-5">Agregar competidor</span>
+    </div>
+</div>
+
+<style>
+    .nav-link.active {
+        color: red;
+    }
+</style>
+
+<div class="row col-10 mx-auto my-5 shadow" style="min-height: 400px;">
+    <div class="col-4">
+        <img src="./estructura/adulto2m.jpg" height="300" style="max-width: 100%; height: 100%; object-fit: cover;">
+    </div>
+    <div class="col-8 position-relative">
+        <div class="nav justify-content-center">
+            <div class="nav-link active" data-bs-toggle="tab" data-bs-target="#paso1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-1-circle" viewBox="0 0 16 16">
+                    <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM9.283 4.002V12H7.971V5.338h-.065L6.072 6.656V5.385l1.899-1.383h1.312Z" />
+                </svg>
+            </div>
+            <span class="mx-3"></span>
+            <div class="nav-link" data-bs-toggle="tab" data-bs-target="#paso2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-2-circle" viewBox="0 0 16 16">
+                    <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM6.646 6.24v.07H5.375v-.064c0-1.213.879-2.402 2.637-2.402 1.582 0 2.613.949 2.613 2.215 0 1.002-.6 1.667-1.287 2.43l-.096.107-1.974 2.22v.077h3.498V12H5.422v-.832l2.97-3.293c.434-.475.903-1.008.903-1.705 0-.744-.557-1.236-1.313-1.236-.843 0-1.336.615-1.336 1.306Z" />
+                </svg>
+            </div>
+        </div>
+        <form id="myForm"> <!-- INICIO FORM -->
+            <div class="tab-content">
+                <div class="tab-pane fade show active" id="paso1" role="tabpanel" aria-labelledby="paso1-tab"> <!-- INICIO CONTENIDO PASO 1 -->
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="apellido">Apellido:</label>
+                            <input class="form-control" type="text" id="apellido" name="apellido">
+                        </div>
+
+                        <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="nombre">Nombre:</label>
+                            <input class="form-control" type="text" id="nombre" name="nombre">
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-12 pt-3">
+                            <label class="form-label" for="dni">DNI:</label>
+                            <input class="form-control" type="number" min="0" id="dni" name="dni">
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="edad">Edad:</label>
+                            <input class="form-control" type="number" min="0" id="edad" name="edad">
+                        </div>
+
+                        <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="email">Email:</label>
+                            <input class="form-control" type="email" id="email" name="email">
+                        </div>
+
+                        <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="paisOrigen">País:</label>
+                            <input class="form-control" type="text" id="paisOrigen" name="paisOrigen">
+                        </div>
+
+                        <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="genero">Género:</label>
+                            <select class="form-control" id="genero" name="genero">
+                                <option disabled selected>Selecciona una opción</option>
+                                <option value="masculino">Masculino</option>
+                                <option value="femenino">Femenino</option>
+                                <option value="otro">Otro</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="position-absolute bottom-0 end-0 mb-3 me-3">
+                        <button class="btn btn-outline-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                            </svg>
+                            Siguiente
+                        </button>
+                    </div>
+                </div> <!-- FIN CONTENIDO PASO 1 -->
+                <div class="tab-pane fade" id="paso2" role="tabpanel" aria-labelledby="paso2-tab"> <!-- INICIO CONTENIDO PASO 2 -->
+                    <div class="row">
+                        <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="legajo">Legajo:</label>
+                            <input class="form-control" type="text" id="legajo" name="legajo">
+                        </div>
+                        <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+                            <label class="form-label" for="rankingNacional">Ranking:</label>
+                            <input class="form-control" type="number" min="0" id="rankingNacional" name="rankingNacional">
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-sm-12  pt-3">
+                            <label class="form-label" for="graduacion">Graduacion:</label>
+                            <select class="form-control" id="graduacion" name="graduacion">
+                            </select>
+                        </div>
+                    </div>
+                    <div class="position-absolute bottom-0 end-0 mb-3 me-3 mt-5">
+                        <button class="btn btn-outline-secondary align-middle">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                            </svg>
+                            Volver
+                        </button>
+                        <button class="btn btn-outline-success">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-earmark-plus-fill" viewBox="0 0 16 16">
+                                <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8.5 7v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 1 0z" />
+                            </svg>
+                            Guardar
+                        </button>
+                    </div>
+                </div> <!-- FIN CONTENIDO PASO 2 -->
+            </div>
+        </form> <!-- FIN FORM -->
+    </div>
+</div>
+
+
+
+<!--<form class="row m-5" id="myForm">
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="legajo">Legajo:</label>
+        <input class="form-control" type="text" id="legajo" name="legajo">
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="apellido">Apellido:</label>
+        <input class="form-control" type="text" id="apellido" name="apellido">
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="nombre">Nombre:</label>
+        <input class="form-control" type="text" id="nombre" name="nombre">
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12 pt-3">
+        <label class="form-label" for="dni">DNI:</label>
+        <input class="form-control" type="number" id="dni" name="dni">
+    </div>
+
+    <div class="col-lg-6 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="email">Email:</label>
+        <input class="form-control" type="email" id="email" name="email">
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="edad">Edad:</label>
+        <input class="form-control" type="number" id="edad" name="edad">
+
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="paisOrigen">País:</label>
+        <input class="form-control" type="text" id="paisOrigen" name="paisOrigen">
+
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="genero">Género:</label>
+        <select class="form-control" id="genero" name="genero">
+            <option value="">Selecciona una opción</option>
+            <option value="masculino">Masculino</option>
+            <option value="femenino">Femenino</option>
+            <option value="otro">Otro</option>
+        </select>
+
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+        <label class="form-label" for="graduacion">Graduacion:</label>
+        <select class="form-control" id="graduacion" name="graduacion">
+        </select>
+
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3">
+
+        <label class="form-label" for="rankingNacional">Ranking:</label>
+        <input class="form-control" type="number" id="rankingNacional" name="rankingNacional">
+    </div>
+
+    <div class="col-lg-3 col-md-6 col-sm-12  pt-3 m-auto">
+
+        <input class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal" type="button" value="Enviar" onclick="convertirEnJSON()">
+        <input class="btn btn-secondary" type="reset" value="Borrar datos">
+    </div>
+
+    <script>
+        function convertirEnJSON() {
+            var form = document.getElementById("myForm");
+            var data = new FormData(form);
+            var object = {};
+            data.forEach(function(value, key) {
+                object[key] = value;
+            });
+            var json = JSON.stringify(object);
+            //console.log(json);
+            cargarDatos(json)
+            /* usando por ahora una cookie */
+            document.cookie = json
+
+        }
+        // Leer datos del archivo JSON
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var myObj = JSON.parse(this.responseText);
+                // Crear selector en el DOM
+                var x = document.getElementById("graduacion");
+                for (var i = 0; i < myObj.length; i++) {
+                    var option = document.createElement("option");
+                    option.text = myObj[i].graduacion;
+                    x.add(option);
+                }
+            }
+        };
+        xmlhttp.open("GET", "graduaciones.json", true);
+        xmlhttp.send();
+    </script>
+
+</form>
+<div id="modal" class="modal" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Competidor agregado</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div id="cuerpoModal" class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+
+            </div>
+        </div>
+    </div>
+</div>-->
+<script src="./agregarDatosModal.js"></script>
+<?php include_once('./estructura/footer.php') ?>
---
+++ TP2/Ejercicio Obligatorio/tablaRegistro.php
@@ -0,0 +1,89 @@
+<?php include_once('./estructura/header.php') ?>
+
+<div class="row p-3 text-light bg-seccion2">
+    <div class="text-center">
+        <span class="display-5">Lista de Competidores Registrados</span>
+    </div>
+</div>
+<div class="row">
+    .<div class="table-responsive">
+        <table id="tabla" class="table table-striped
+                table-hover	
+                table-borderless
+                table-primary
+                align-middle">
+            <thead class="table-light">
+                <tr>
+                    <th>Legajo</th>
+                    <th>Apellido</th>
+                    <th>Nombre</th>
+                    <th>DU</th>
+                    <th>Email</th>
+                    <th>Edad</th>
+                    <th>País de origen</th>
+                    <th>Género</th>
+                    <th>Graduación</th>
+                    <th>Ranking nacional</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr class="table-primary">
+                </tr>
+            </tbody>
+            <tfoot>
+
+            </tfoot>
+        </table>
+    </div>
+
+</div>
+
+
+<script>
+    var datos = JSON.parse(document.cookie);
+    console.log(datos)
+
+    var tabla = document.getElementById("tabla");
+    var row = tabla.insertRow();
+    for (var key in datos) {
+        var cell = row.insertCell();
+        cell.innerHTML = datos[key];
+    }
+
+
+    /* ESTO ES SOLO UNA IDEA */
+    /* QUE NO SIRVE */
+    /* fetch("tablaDatos.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        var tabla = document.getElementById("tabla");
+        for (var i = 0; i < data.length; i++) {
+            var fila = tabla.insertRow(i+1);
+            var celda1 = fila.insertCell(0);
+            var celda2 = fila.insertCell(1);
+            var celda3 = fila.insertCell(2);
+            var celda4 = fila.insertCell(3);
+            var celda5 = fila.insertCell(4);
+            var celda6 = fila.insertCell(5);
+            var celda7 = fila.insertCell(6);
+            var celda8 = fila.insertCell(7);
+            var celda9 = fila.insertCell(8);
+            var celda10 = fila.insertCell(9);
+            
+            celda1.innerHTML = data[i].legajo;
+            celda2.innerHTML = data[i].apellido;
+            celda3.innerHTML = data[i].nombre;
+            celda4.innerHTML = data[i].du;
+            celda5.innerHTML = data[i].fechaNacimiento;
+            celda6.innerHTML = data[i].paisOrigen;
+            celda7.innerHTML = data[i].graduacion;
+            celda8.innerHTML = data[i].rankingNacional;
+            celda9.innerHTML = data[i].email;
+            celda10.innerHTML = data[i].genero;
+        }
+    }); */
+</script>
+
+
+<?php include_once('./estructura/footer.php') ?>
