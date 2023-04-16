986191287355c76f17e16b0ceb0bba86fd13d1b7
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-04-16 02:25:15 -0300

    tabla secundaria con algunos retoques

--- TP2/Ejercicio Obligatorio/tablaRegistro copy 3.php
+++ TP2/Ejercicio Obligatorio/tablaRegistro copy 3.php
@@ -7,11 +7,20 @@
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.4.1/js/dataTables.responsive.min.js"></script>
+    <style>
+        table.dataTable.dtr-inline.collapsed>tbody>tr>td.dtr-control:before, table.dataTable.dtr-inline.collapsed>tbody>tr>th.dtr-control:before{
+            background-color: orangered;
+            font-weight: bolder;
+        }
+        table.dataTable.dtr-inline.collapsed>tbody>tr.parent>td.dtr-control:before, table.dataTable.dtr-inline.collapsed>tbody>tr.parent>th.dtr-control:before{
+            background-color: #D1A637;
+        }
+    </style>
 </head>
 
 
 
-<div class="row p-3 text-light bg-seccion2">
+<div class="row p-3 text-light bg-seccion2 mb-3">
     <div class="text-center">
         <span class="display-5">Lista de Competidores Registrados</span>
     </div>
@@ -19,8 +28,8 @@
 
 <body>
 
-    <table id="myTable" class="table table-striped table-hover	
-                table-borderless display responsive nowrap" style="width:100%">
+    <table id="myTable" class="table table-striped table-hover table-borderless display responsive nowrap"
+        style="width:100%">
     </table>
 
     <script type="text/javascript">
@@ -34,64 +43,84 @@
                 data: dataResult,
                 columnDefs: columns,
                 order: [[7, 'desc']], /* orden descendiente rank */
-                pageLength: 10
+                pageLength: 10,
+                "language": {
+                    "search": "Buscar:",
+                    "lengthMenu": "Ver _MENU_ entradas",
+                    "paginate": {
+                        "first": "Primero",
+                        "last": "Ultimo",
+                        "next": "Siguiente",
+                        "previous": "Anterior"
+                    },
+                    "infoEmpty":      "Viendo 0 a 0 de 0 entradas",
+                    "info":      "Viendo _START_ a _END_ de _TOTAL_ entradas",
+                },
             })
         }
 
         async function datos() {
-            const data = await fetch("tablaDatos.json")
+            const data = await fetch("tablaDatos2.json")
                 .then(value => value.json())
                 .then(value => value)
 
             const columns = [{
                 targets: 0,
-                title: "legajo",
+                title: "Legajo",
                 data: "legajo"
             },
             {
                 targets: 1,
-                title: "apellido",
+                title: "Apellido",
                 data: "apellido",
             },
             {
                 targets: 2,
-                title: "nombre",
+                title: "Nombre",
                 data: "nombre",
             },
             {
                 targets: 3,
-                title: "du",
-                data: "du"
+                title: "Pais",
+                data: "paisOrigen",
+                render: (data, type, r, m) => {
+                    return (type == "display") ? `<img src="svg/${data}.svg" alt="${data}" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Tooltip on top" width="20px"> ${data}</img>` : data
+                }
             },
             {
                 targets: 4,
-                title: "fechaNacimiento",
-                data: "fechaNacimiento",
-            },
-            {
-                targets: 5,
-                title: "paisOrigen",
-                data: "paisOrigen",
-
-            },
-            {
-                targets: 6,
-                title: "graduacion",
-                data: "graduacion",
-            },
-            {
-                targets: 7,
-                title: "rankingNacional",
+                title: "Rank Nacional",
                 data: "rankingNacional",
             },
             {
+                targets: 5,
+                title: "Movimiento",
+                data: "du",
+                render: (data, type, r, m) => {
+                    return (type == "display") ? `<button class="btn btn-dark text-white bolder">${data}</button>` : data
+                }
+            },
+            {
+                targets: 6,
+                title: "Grado",
+                data: "graduacion",
+                render: (data, type, r, m) => {
+                    return (type == "display") ? `<button class="btn btn-dark text-white bolder">${data}</button>` : data
+                }
+            },
+            {
+                targets: 7,
+                title: "Fecha de Nacimiento",
+                data: "fechaNacimiento",
+            },
+            {
                 targets: 8,
-                title: "email",
+                title: "Email",
                 data: "email",
             },
             {
                 targets: 9,
-                title: "genero",
+                title: "Genero",
                 data: "genero",
             }
             ]
