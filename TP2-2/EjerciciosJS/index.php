8e6812abb29638e0f12f0bd086e1e1d833d2c3a4
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-04-18 19:21:10 -0300

    estoy toqueteando el nav para que se abra desde abajo el burger

---
+++ TP2-2/EjerciciosJS/index.php
@@ -0,0 +1,19 @@
+<?php include_once('estructura/header.php')?>
+
+
+<div class="tab-content" id="myTabContent">
+  <div class="tab-pane fade show " id="seccion1-pane" role="tabpanel" aria-labelledby="seccion1-tab" tabindex="0">
+  <?php include_once('seccion1.php')?>
+  </div>
+  <div class="tab-pane fade" id="seccion2-pane" role="tabpanel" aria-labelledby="seccion2-tab" tabindex="0">
+  <?php include_once('seccion2.php')?>
+  </div>
+  <div class="tab-pane fade" id="seccion3-pane" role="tabpanel" aria-labelledby="seccion3-tab" tabindex="0">
+  <?php include_once('seccion3.php')?>
+  </div>
+  <div class="tab-pane fade" id="seccion4-pane" role="tabpanel" aria-labelledby="seccion4-tab" tabindex="0">
+  <?php include_once('seccion4.php')?>
+  </div>
+</div>
+
+<?php include_once('estructura/footer.php')?>
--- TP2-2/EjerciciosJS/seccion4.php
+++ TP2-2/EjerciciosJS/seccion4.php
@@ -1,10 +1,6 @@
-<?php include_once('./estructura/header.php') ?>
+
 <div class="row p-3 text-light bg-seccion2">
     <div class="text-center">
         <span class="display-5">Seccion 4 </span>
     </div>
 </div>
-
-
-
-<?php include_once('./estructura/footer.php') ?>
