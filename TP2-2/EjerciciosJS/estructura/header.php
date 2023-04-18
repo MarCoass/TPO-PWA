b7ca7a9c2a7897c7d5aa0123306c36efee4fbc02
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-04-17 22:38:11 -0300

    Carga parte 2

---
+++ TP2-2/EjerciciosJS/estructura/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Poomsae Reconocido Individual Élite</title>
+
+    <!-- ICON -->
+    <link rel="icon" type="image\x-icon" href=".\estructura\img\logo.ico">
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Jquery 3.6.4 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+
+<body>
+    
+    <div class="container-fluid ">
+        <nav class="navbar navbar-expand-lg m-3">
+            <div class="container-fluid">
+                <img src="./estructura/img/World_Taekwondo.png" alt="" width="100px" /><span class="fs-4 ms-3">Poomsae Reconocido</span>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse ms-3" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
+                                    <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z" />
+                                </svg>
+                                Agregar Competidor
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="tablaRegistro.php">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-table" viewBox="0 0 16 16">
+                                    <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z" />
+                                </svg>
+                                Ver Competidores
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
---
+++ TP2-2/EjerciciosJS/index.php
@@ -0,0 +1,10 @@
+<?php include_once('./estructura/header.php') ?>
+<div class="row p-3 text-light bg-seccion2">
+    <div class="text-center">
+        <span class="display-5">Ejercicios API, DOM y CSS </span>
+    </div>
+</div>
+
+
+
+<?php include_once('./estructura/footer.php') ?>
