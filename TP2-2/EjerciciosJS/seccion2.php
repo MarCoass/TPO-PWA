2897f9ce0dc11a1b385d68253b14fdf9fab3e1ff
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-04-21 10:38:29 -0300

    seccionn 2

--- TP2-2/EjerciciosJS/seccion2.php
+++ TP2-2/EjerciciosJS/seccion2.php
@@ -4,3 +4,7 @@
         <span class="display-5">Seccion 2 </span>
     </div>
 </div>
+
+<a class="d-sm-none d-block" href="https://youtu.be/CpEGREBGh2E">Ver video de la última competencia internacional de Poomsae</a>
+<iframe class="d-sm-block d-none" width="560" height="315" src="https://youtu.be/CpEGREBGh2E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen loading="lazy"></iframe>
+
