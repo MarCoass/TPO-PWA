40ea9fb1b82bc28a2977ae23eb5311be8272e41a
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-04-24 14:24:15 -0300

    tooltip para el genero

--- TPO/util/js/paginacion.js
+++ TPO/util/js/paginacion.js
@@ -11,6 +11,20 @@ function obtenerCompetidores() {
   return competidoresObtenidos;
 }
 
+function obtenerGenero(generoCompetidor) {
+  icono = "";
+  switch (generoCompetidor.toLowerCase()) {
+    case "masculino":
+      icono += "<svg xmlns='http://www.w3.org/2000/svg' data-bs-toggle='tooltip' title='Masculino' width='16' height='16' fill='currentColor' class='bi bi-gender-male' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2H9.5zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8z'/></svg>";
+      break;
+    case "femenino":
+      icono += "<svg xmlns='http://www.w3.org/2000/svg' data-bs-toggle='tooltip' title='Femenino' width='16' height='16' fill='currentColor' class='bi bi-gender-female' viewBox='0 0 16 16'><path fill-rule='evenodd' d='M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5z'/></svg>"
+      break;
+  }
+
+  return icono;
+}
+
 $(function () {
   let container = $("#tabla");
 
@@ -62,13 +76,14 @@ $(function () {
         celdaPais.innerHTML =
           '<img src="../util/svg/' +
           competidor.paisOrigen +
-          '.svg" alt="..." data-bs-toggle="tooltip" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="' +
+          '.svg" alt="..." data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="' +
           competidor.paisOrigen +
           '" width="20px">';
         celdaPais.classList.add("d-none", "d-lg-table-cell");
 
         const celdaGenero = nuevaFila.insertCell();
-        celdaGenero.textContent = competidor.genero;
+        iconoGenero = obtenerGenero(competidor.genero);
+        celdaGenero.innerHTML = iconoGenero
         celdaGenero.classList.add("d-none", "d-lg-table-cell");
 
         const celdaGraduacion = nuevaFila.insertCell();
