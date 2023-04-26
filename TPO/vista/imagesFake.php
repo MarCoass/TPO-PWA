36691002ba56f1ce65ba9303b3f3b0bd13655b98
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-04-26 14:49:51 -0300

    Update imagesFake.php

--- TPO/vista/imagesFake.php
+++ TPO/vista/imagesFake.php
@@ -1,4 +1,4 @@
-<div class="row my-3 justify-content-center" id="contenedorImagenesFake">
+<div class="row my-3 justify-content-center parpadear" id="contenedorImagenesFake">
     <?php include("./estructura/cardFake.php") ?>
     <?php include("./estructura/cardFake.php") ?>
     <?php include("./estructura/cardFake.php") ?>
