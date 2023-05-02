5670610e5b2e5588a486610669d52ca44dedc17a
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-05-02 17:24:30 -0300

    sql actualizado

--- TPO/tpo-pwa.sql
+++ TPO/tpo-pwa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-04-2023 a las 21:56:06
+-- Tiempo de generación: 02-05-2023 a las 22:23:36
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 
