d2ab515738a44c3badb886a4ece4c4cf68930e62
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-05-03 10:29:33 -0300

    encapsulado modal ver detalle imagen

--- TPO/vista/index.php
+++ TPO/vista/index.php
@@ -136,27 +136,8 @@
 
 <?php include_once("./imagesFake.php") ?> <!-- IMAGENES FAKE -->
 
-<!-- MODAL VER DETALLE IMAGEN -->
-<div class="modal fade" id="modalImagen" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Información de la Imagen <span class="text-uppercase" id="nombreRandom"></span></h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <p>ID del Albúm: <span id="idAlbumImagen"></span></p>
-                <p>ID de la Imagen: <span id="idImagen"></span></p>
-                <p>Título: <span id="tituloImagen"></span></p>
-                <p>Thumbnail URL: <span id="thumbUrlImagen"></span></p>
-                <p>URL: <span id="urlImagen"></span></p>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Cerrar Modal</button>
-            </div>
-        </div>
-    </div>
-</div>
+<?php include_once("./estructura/modalVerImagen.php") ?> <!-- MODAL VER DETALLE IMAGEN -->
+
 <!-- 
 
  -->
