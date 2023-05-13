c636c03c70d7adc63f9777330039ae7a1bd76a65
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-05-13 10:43:23 -0300

    Create arreglandoDatos.blade.php

---
+++ TP-Laravel/resources/views/arreglandoDatos.blade.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::get('/', function () {
+  return [
+    'materiaYTema' => [
+        'PWA',
+        'Frameworks',
+        'Introduccion a framework con Laravel',
+        ]
+    ];
+});
