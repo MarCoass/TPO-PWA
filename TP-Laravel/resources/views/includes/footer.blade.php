bb43b1b42cbc7e7f9e4d7d133bcf76b8d2f5a3d2
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-05-13 15:36:11 -0300

    estructura base, instalados localmente bootstrap 5 e iconos

---
+++ TP-Laravel/resources/views/includes/footer.blade.php
@@ -0,0 +1,69 @@
+<div class="bg-danger">
+    <div class="bg-danger row d-flex" id="footer">
+        <section class="mt-3" style="user-select: none;">
+            <footer class="text-center text-white">
+                <div class="container">
+                    <div class="row text-center">
+                        <div class="col">
+                            <a href="https://github.com/MarCoass">
+                                <button type="button" class="btn btn-outline-dark">
+                                    <h5 class="mx-2"><i class="bi bi-github"></i></h5>
+                                </button>
+                            </a>
+                            <h5>Martina Coassin</h5>
+                            <p>FAI - 2542</p>
+                        </div> |
+                        <div class="col">
+                            <a href="https://github.com/BraianCenturion2001" style="text-decoration:none;">
+                                <button type="button" class="btn btn-outline-dark">
+                                    <h5 class="mx-2"><i class="bi bi-github"></i></h5>
+                                </button>
+                            </a>
+                            <h5>Braian Centurión</h5>
+                            <p>FAI - 3001</p>
+                        </div> |
+                        <div class="col">
+                            <a href="https://github.com/matiasnqn16">
+                                <button type="button" class="btn btn-outline-dark">
+                                    <h5 class="mx-2"><i class="bi bi-github"></i></h5>
+                                </button>
+                            </a>
+                            <h5>Matias Farfan</h5>
+                            <p>FAI - 1842</p>
+                        </div> |
+                        <div class="col">
+                            <a href="#">
+                                <button type="button" class="btn btn-outline-dark">
+                                    <h5 class="mx-2"><i class="bi bi-github"></i></h5>
+                                </button>
+                            </a>
+                            <h5>Alhue Paillamilla</h5>
+                            <p>FAI - 0000</p>
+                        </div> |
+                        <div class="col">
+                            <a href="#" style="text-decoration:none;">
+                                <button type="button" class="btn btn-outline-dark">
+                                    <h5 class="mx-2"><i class="bi bi-github"></i></h5>
+                                </button>
+                            </a>
+                            <h5>Gonzalo Olmos</h5>
+                            <p>FAI - 0000</p>
+                        </div> |
+                        <div class="col">
+                            <a href="#">
+                                <button type="button" class="btn btn-outline-dark">
+                                    <h5 class="mx-2"><i class="bi bi-github"></i></h5>
+                                </button>
+                            </a>
+                            <h5>Gustavo Oliveros</h5>
+                            <p>FAI - 0000</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mb-3">
+                    Programación Web Avanzada - 2023 <img src="{{ asset('images/logo.ico') }}" class="mx-2" alt="FAI" title="FAI" width="40" height="40"> Universidad Nacional del Comahue
+                </div>
+            </footer>
+        </section>
+    </div>
+</div>
