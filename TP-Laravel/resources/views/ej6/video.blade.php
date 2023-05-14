956360389642f9877b8a6747e9ce5838a27d4f4d
Gonzalo Olmos <gonzaloolmos1998@gmail.com>
2023-05-14 18:09:56 -0300

    agregando seccion video

--- TP-Laravel/resources/views/ej6/video.blade.php
+++ TP-Laravel/resources/views/ej6/video.blade.php
@@ -5,9 +5,22 @@
 @endsection
 
 @section('encabezado')
-    Video
+<h1 class="display-2">Video</h1> 
 @endsection
 
 @section('contenido')
-    Este es texto de relleno para la vista Video
+
+<div class="text-center">
+    <div class="row my-5 d-block d-md-none">
+        <div class="col-12">
+            <a href="https://www.youtube.com/watch?v=ermNRSmkGF8" target="_blank" class="btn btn-danger">Haga clic acá para ver un vídeo</a>
+        </div>
+    </div>
+    
+    <div class="row my-5 d-none d-md-block">
+        <div class="col-12">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ermNRSmkGF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
 @endsection
