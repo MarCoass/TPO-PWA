eb484a91a9b7084f2c84909f73d54d04d8f5e301
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-05-15 16:49:38 -0300

    cambio de tema

--- TP-Laravel/public/js/tema.js
+++ TP-Laravel/public/js/tema.js
@@ -6,7 +6,10 @@ export const cambio_de_tema = (tema) => {
        
     if(tema === "dark"){
 
-      document.body.style.backgroundColor = "#000000";
+      //document.body.style.backgroundColor = "var(--bs-dark)";
+
+      document.body.classList.add('text-bg-dark')
+      document.body.classList.remove('text-bg-light')
 
       let negro = document.getElementById("cambiarVista_negro");
       negro.style.display = "none";
@@ -16,7 +19,9 @@ export const cambio_de_tema = (tema) => {
 
       localStorage.setItem("background", "dark");
     }else{
-      document.body.style.backgroundColor = "#FFFFFF";
+      //document.body.style.backgroundColor = "#FFFFFF";
+      document.body.classList.remove('text-bg-dark')
+      document.body.classList.add('text-bg-light')
 
       let negro = document.getElementById("cambiarVista_negro");
       negro.style.display = "block";
--- TP-Laravel/resources/views/includes/modalVerImagen.php
+++ TP-Laravel/resources/views/includes/modalVerImagen.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="modalImagen" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalImagen" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-theme="light">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
