7517d47baaad448e47edf14faac30f0aae68da38
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-05-15 16:21:47 -0300

    link home logo

--- TP-Laravel/resources/views/includes/navbar.blade.php
+++ TP-Laravel/resources/views/includes/navbar.blade.php
@@ -1,6 +1,9 @@
 <header class="sticky-top border-bottom border-danger border-2">
     <nav id="menuHamburguesa" class="navbar navbar-expand-xxxl p-3  text-bg-light">
-        <img src="{{ asset('images/World_Taekwondo.png') }}" alt="logo TKD" width="100px" /><span class="fs-4 ms-3 tituloPag">Poomsae
+        <a href="/">
+            <img src="{{ asset('images/World_Taekwondo.png') }}" alt="logo TKD" width="100px" />
+        </a>
+        <span class="fs-4 ms-3 tituloPag">Poomsae
             Reconocido</span>
             <button id="cambiarVista_negro" class="btn btn-dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-moon-fill mb-1" viewBox="0 0 16 16">
                 <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z" />
