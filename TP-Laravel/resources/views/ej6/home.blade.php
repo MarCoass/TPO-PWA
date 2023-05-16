1c09869e9df86db7ab20825c66037e2ffa0fe6c1
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-05-15 21:36:24 -0300

    fix: Home

--- TP-Laravel/resources/views/ej6/home.blade.php
+++ TP-Laravel/resources/views/ej6/home.blade.php
@@ -9,5 +9,21 @@
 @endsection
 
 @section('contenido')
-    Este es texto de relleno para la vista Home
+<h1>Seccion 1</h1>
+@include('cronometro');
+
+<h1>Secciion 2</h1>
+
+@include('video');
+
+<h1>Seccion 3 </h1>
+@include('tablaCompetidores');
+
+<h1> Seccion 4 </h1>
+Formulario competidor
+
+<h1>Seccion 5</h1>
+
+@include('imagenesRandom');
+
 @endsection
