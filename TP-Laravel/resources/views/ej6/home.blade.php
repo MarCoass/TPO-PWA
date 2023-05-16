5168000e82309ee9c2dbd0eecf3996e4f09a4d10
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-05-16 20:28:13 -0300

    nope

--- TP-Laravel/resources/views/ej6/home.blade.php
+++ TP-Laravel/resources/views/ej6/home.blade.php
@@ -9,21 +9,5 @@
 @endsection
 
 @section('contenido')
-<h1>Seccion 1</h1>
-@include('cronometro');
-
-<h1>Secciion 2</h1>
-
-@include('video');
-
-<h1>Seccion 3 </h1>
-@include('tablaCompetidores');
-
-<h1> Seccion 4 </h1>
-Formulario competidor
-
-<h1>Seccion 5</h1>
-
-@include('imagenesRandom');
-
+texto
 @endsection
