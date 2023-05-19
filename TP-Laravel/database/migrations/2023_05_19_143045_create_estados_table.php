3b5d8d3d891bd5e77785874de54b64fc22123947
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-05-19 13:43:51 -0300

    agregada la clave foranea de idPais

---
+++ TP-Laravel/database/migrations/2023_05_19_143045_create_estados_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estados', function (Blueprint $table) {
+            $table->id('idEstado');
+            $table->unsignedBigInteger('idPais');
+            $table->string('nombreEstado');
+            $table->timestamps();
+
+            $table->foreign('idPais')->references('idPais')->on('paises');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estados');
+    }
+};
