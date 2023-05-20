821b18d61f75cb304c0a506de446b404d86937e4
Matias Farfan <61336105+matiasnqn16@users.noreply.github.com>
2023-05-20 01:41:21 -0300

    migrate Users

---
+++ TP-Laravel/database/migrations/2023_05_20_000000_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+/* 
+
+SE CREO LA TABLA CON EL NOMBRE USER PARA QUE SEA MAS 
+COMPRENSIBLE PARA AUTH DE LARAVEL JUNTO
+CON EL CAMPO PASSWORD COMO CONTRASEÑA
+
+*/
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            /* A ID LO DEJE SIN NOMBRAR PARA NO GENERAR CONFLICTOS CON SU CONFIGURACION YA ESTABLECIDA */
+            $table->id();
+            $table->string('nombre')->nullable();
+            $table->string('apellido')->nullable();
+            $table->string('correo')->unique();
+            $table->string('usuario')->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('idRol')->nullable();
+            /* $table->unsignedBigInteger('idRol'); */
+            $table->rememberToken();
+            $table->timestamps();
+
+            $table->foreign('idRol')->references('idRol')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
