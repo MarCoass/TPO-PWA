be774260a68da839fee4d1e77016b089c2ce3a18
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-05-27 12:00:52 -0300

    fix: mejoras en tabla de competenciaCompetidor

--- TP-Laravel/database/migrations/91_create_competencia_competidors_table.php
+++ TP-Laravel/database/migrations/91_create_competencia_competidors_table.php
@@ -17,11 +17,15 @@ return new class extends Migration
             $table->id();
             $table->unsignedBigInteger('idCompetidor');
             $table->unsignedBigInteger('idCompetencia');
+            $table->unsignedBigInteger('idPoomsae');
+            $table->decimal('puntaje');
+            $table->int('contadorPasadas');
             $table->string('estado');
             $table->timestamps();
 
             $table->foreign('idCompetidor')->references('idCompetidor')->on('competidores');
             $table->foreign('idCompetencia')->references('idCompetencia')->on('competencias');
+            $table->foreign('idPoomsae')->references('idPoomsae')->on('poomsae');
 
         });
     }
@@ -38,6 +42,9 @@ return new class extends Migration
         });
         Schema::table('competencias', function (Blueprint $table) {
             $table->dropForeign(['idCompetencia']);
+        });
+        Schema::table('poomsae', function (Blueprint $table) {
+            $table->dropForeign(['idPoomsae']);
         });
         Schema::dropIfExists('competencia_competidors');
     }
