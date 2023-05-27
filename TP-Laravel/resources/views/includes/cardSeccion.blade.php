37f264ef83d7b9af08b0a1ba30a1256bde122ce8
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-05-26 21:11:09 -0300

    Imagenes<-tp anterior, borrados

--- TP-Laravel/resources/views/includes/cardSeccion.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="col-lg-4">
-    <img class="bd-placeholder-img rounded-circle" src="@yield('imgSeccion')" alt="" srcset="">
-    <h2 class="fw-normal">@yield('tituloSeccion')</h2>
-    <p>@yield('textoSeccion')</p>
-    <p><a class="btn btn-secondary" href=@yield('urlSeccion')>Ir »</a></p>
-</div><!-- /.col-lg-4 -->
