5da3a15b2365c6e0e19bde9f2f5a4d929eec1a9a
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-06-01 12:44:47 -0300

    se puede descargar la imagen
    :heart:

--- TP-Laravel/resources/views/presentacion/video.blade.php
+++ TP-Laravel/resources/views/presentacion/video.blade.php
@@ -5,37 +5,49 @@
 @endsection
 
 @section('encabezado')
-Presentacion del Torneo
+    Presentacion del Torneo
 @endsection
 
 @section('contenido')
+    <div class="text-center">
+        <div class="row my-5 d-block d-md-none">
+            <div class="col-12">
+                <a href="https://www.youtube.com/watch?v=ermNRSmkGF8" target="_blank" class="btn btn-danger">Haga clic acá para
+                    ver un vídeo</a>
+            </div>
+        </div>
 
-<div class="text-center">
-    <div class="row my-5 d-block d-md-none">
-        <div class="col-12">
-            <a href="https://www.youtube.com/watch?v=ermNRSmkGF8" target="_blank" class="btn btn-danger">Haga clic acá para ver un vídeo</a>
+        <div class="row my-5 d-none d-md-block">
+            <div class="col-12">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ermNRSmkGF8"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="row">
+            <section class="col-12 col-md-4">
+                <h3>Invitacion</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non necessitatibus earum, velit inventore
+                    assumenda cupiditate molestiae ullam, ea nobis esse totam aliquid, vitae perspiciatis accusantium quae!
+                    Voluptate inventore porro quod?</p>
+            </section>
+            <section class="col-12 col-md-4">
+                <h3>Bases</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non necessitatibus earum, velit inventore
+                    assumenda cupiditate molestiae ullam, ea nobis esse totam aliquid, vitae perspiciatis accusantium quae!
+                    Voluptate inventore porro quod?</p>
+            </section>
+            <section class="col-12 col-md-4">
+                <div class="d-flex flex-row justify-content-center align-items-center">
+                <p class="display-5 mx-3">Flyer</p>
+                <a href="{{ asset('storage/' . $competencia->flyer) }}" download>
+                    <button class="btn btn-outline-dark">Descargar<i class="bi bi-download px-2"></i></button>
+                  </a>
+                  
+                </div>
+                <img class="img-fluid" src="{{ asset('storage/' . $competencia->flyer) }}" alt="">
+            </section>
         </div>
     </div>
-    
-    <div class="row my-5 d-none d-md-block">
-        <div class="col-12">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ermNRSmkGF8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-    </div>
-    <div class="row">
-        <section class="col-12 col-md-4">
-            <h3>Invitacion</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non necessitatibus earum, velit inventore assumenda cupiditate molestiae ullam, ea nobis esse totam aliquid, vitae perspiciatis accusantium quae! Voluptate inventore porro quod?</p>
-        </section>
-        <section class="col-12 col-md-4">
-            <h3>Bases</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non necessitatibus earum, velit inventore assumenda cupiditate molestiae ullam, ea nobis esse totam aliquid, vitae perspiciatis accusantium quae! Voluptate inventore porro quod?</p>
-        </section>
-        <section class="col-12 col-md-4">
-            <h3>Flyer</h3>
-           
-            <img src="{{asset('storage/'.$competencia->flyer)}}" alt="">
-        </section>
-    </div>
-</div>
 @endsection
