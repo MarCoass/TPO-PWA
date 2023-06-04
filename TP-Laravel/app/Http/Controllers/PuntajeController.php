000b1c220ca1b6ffdd831d8fb508e8f2095ee55e
Martina Coassin <martina.coassin@est.fi.uncoma.edu.ar>
2023-06-04 19:09:18 -0300

    Update PuntajeController.php
    el select solo muestra las competencias que ya tengan todos sus jueces

--- TP-Laravel/app/Http/Controllers/PuntajeController.php
+++ TP-Laravel/app/Http/Controllers/PuntajeController.php
@@ -103,7 +103,7 @@ class PuntajeController extends Controller
         //filtrar competencias por incripcion de juez
         $user = auth()->user();
         $competencias = Competencia::select('competencias.*')
-        ->join('competenciajueces', 'competencias.idCompetencia', '=', 'competenciajueces.idCompetencia')->where('competenciajueces.idJuez','=',$user->id)->where('competenciajueces.estado','=','1')->get();
+        ->join('competenciajueces', 'competencias.idCompetencia', '=', 'competenciajueces.idCompetencia')->where('competenciajueces.idJuez','=',$user->id)->where('competenciajueces.estado','=','1')->where('estadoJueces','=', 1)->get();
 
         return view('puntuador.index', compact('competencias'));
     }
