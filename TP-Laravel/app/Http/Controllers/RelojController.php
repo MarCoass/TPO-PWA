c91fe17620ef6665f76a037a3fd3d02cbea472ee
patito <micaela.paillamilla@est.fi.uncoma.edu.ar>
2023-06-11 13:06:42 -0300

    Avances en el reloj

---
+++ TP-Laravel/app/Http/Controllers/RelojController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class RelojController extends Controller
+{
+    public function index()
+    {
+        return view('cronometro.index');
+    }
+
+    public function start(Request $request)
+    {
+        $request->session()->put('cronometro_start', now());
+        return response()->json(['success' => true]);
+    }
+
+    public function stop(Request $request)
+    {
+        $start = $request->session()->get('cronometro_start');
+        $duration = now()->diffInSeconds($start);
+        $request->session()->forget('cronometro_start');
+        return response()->json(['success' => true, 'duration' => $duration]);
+    }
+
+    /**
+     * ALGO ASI HAY QUE HACER CUANDO BOTEN LOS JUECES SI ESQUE SE LES TERMINO EL TIEMPO
+     * ES UN EJEMPLO
+     * class VotacionController extends Controller{
+    public function store(Request $request)
+    {
+        if ($request->session()->has('cronometro_start')) {
+            return response()->json(['error' => 'El cronómetro está activo']);
+        }
+        // Lógica para guardar las votaciones
+        return response()->json(['success' => true]);
+    }
+    }
+    */
+
+}
