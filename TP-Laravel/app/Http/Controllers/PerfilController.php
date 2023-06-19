73262a7b1fe79cb7ba4ee31392a39a3c0a6028a5
BraianCenturion2001 <braiankrayan@hotmail.com>
2023-06-19 16:18:51 -0300

    creacion del controlador PerfilController

---
+++ TP-Laravel/app/Http/Controllers/PerfilController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PerfilController extends Controller
+{
+    public function index(){
+        
+    }
+}
