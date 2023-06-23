0fabad0101105869d1aaa3d0f388d2f4ad20fc87
FarfanMatias <matias.farfan@est.fi.uncoma.edu.ar>
2023-06-23 19:04:41 -0300

    fix botones home invitados

--- TP-Laravel/resources/views/home/invitados/vistaInvitados.blade.php
+++ TP-Laravel/resources/views/home/invitados/vistaInvitados.blade.php
@@ -6,28 +6,31 @@
         <h1 class="display-3">Bienvenido a nuestra plataforma web NeuPoom</h1>
     </div>
 
-    <div class="">
-        <div class="seccion_box row justify-content-around">
-            <div class="seccion_item">
-                <a href="{{ route('login.perform') }}" class="seccion-item_link">
-                    <div class="seccion-item_bg">
+    <div class="row justify-content-around">
+        <div class="format-container">
+            <div class="seccion_box">
+                <div class="seccion_item">
+                    <a href="{{ route('login.perform') }}" class="seccion-item_link">
+                        <div class="seccion-item_bg">
 
-                    </div>
-                    <div class="seccion-item_title">
-                        Iniciar sesión.
-                    </div>
-                </a>
-            </div>
+                        </div>
+                        <div class="seccion-item_title">
+                            Iniciar sesión.
+                        </div>
+                    </a>
+                </div>
 
-            <div class="seccion_item">
-                <a href="{{ route('registro.perform') }}" class="seccion-item_link">
-                    <div class="seccion-item_bg"></div>
-                    <div class="seccion-item_title">
-                        Registrarse.
-                    </div>
-                </a>
+                <div class="seccion_item">
+                    <a href="{{ route('registro.perform') }}" class="seccion-item_link">
+                        <div class="seccion-item_bg"></div>
+                        <div class="seccion-item_title">
+                            Registrarse.
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
+
 </div>
 
